9ed16110b837de84d813d15317a5eff47226de38
Nicolas Pedrazzi <nicolas.pedrazzi@hotmail.com>
2026-03-21 10:19:05 +0100

    added sqlite

---
+++ db/dbIndex.ts
@@ -0,0 +1,22 @@
+import { app } from 'electron';
+import path from 'node:path';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+export async function initDb() {
+  const db = await open({
+    filename: path.join(app.getPath('userData'), 'thinkbox.db'),
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+    CREATE TABLE notes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT NOT NULL,
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+      modified_at TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+
+  return db;
+}
